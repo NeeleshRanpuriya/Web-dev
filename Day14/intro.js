3fa292fcf82e3a28a5e1865da97b8930095a8be3
NeeleshRanpuriya <Neeleshranpuriya@gmail.com>
2025-10-02 22:06:45 +0530

    Java Script Basics

---
+++ Day14/intro.js
@@ -0,0 +1,17 @@
+let Age=25.5;
+const name1="Neelesh";
+let Job=null;
+let relation_status=undefined;
+relation_status="Single"
+// name1="amit"
+
+// Undifine variables Declare
+let b=console.log(Job);
+// Age*=2;   Assignment Operator
+console.log(Age)
+console.log(b)
+console.log(typeof(Age));
+console.log(typeof(name1));
+console.log(typeof(Job));
+console.log(typeof(relation_status));
+console.log(typeof b)
---
+++ Day15/App.js
@@ -0,0 +1,4 @@
+let pencilPrice=10;
+let erasorPrice=5;
+output=`Total pay ${pencilPrice+erasorPrice} Rupess`
+console.log(`Total pay ${pencilPrice+erasorPrice} Rupes`)
